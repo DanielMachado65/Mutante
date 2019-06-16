68900f0864dbf0d2066a322a674d9778bd833246
DanielMachado65 <38213401+DanielMachado65@users.noreply.github.com>
2019-06-16 15:22:35 -0300

    update
    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'login', to: 'users#show'
+  post 'login', to: 'users#show'
   post 'create', to: 'users#create'
 
   resources :users
   resources :mutantes
 
-  root 'users#show'
+  root 'mutantes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
